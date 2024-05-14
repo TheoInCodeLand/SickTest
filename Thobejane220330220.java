7268f370d6bca07cd902c5728c04637ae014dbff
Theophilus Thobejane <thobejanetheo@gmail.com>
2024-05-14 08:34:26 +0200

    Updated Thobejane220330220.java file with additional lines

--- Thobejane220330220.java
+++ Thobejane220330220.java
@@ -1,5 +1,8 @@
 public class Thobejane220330220 {
 	public static void main(String[] args) {
 		System.out.println("Hello, This our makeup test!!!");
+		System.out.println("This is an updated version of the class file.");
+		System.out.println("This is the third amd final line to be added");
 	}
+
 }
